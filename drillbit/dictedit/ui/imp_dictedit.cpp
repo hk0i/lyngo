30019f36e097eba266f3fd8541b067feee90a42e
hk0i <hk0i@80cae6e0-d849-4ddc-adcc-ad84e20aa495>
2011-01-09 19:48:50 +0000

    added save and save as features. clicking on a list item in the dictionary will populate the data in the fields for editing (editing feature is to be implemented)
    created a destructor for the ui to clean up the _dictionary property.
    created an update_title function to update the titlebar with appropriate
    information (filename, appname etc)
    
    
    
    git-svn-id: https://lyngo.svn.sourceforge.net/svnroot/lyngo/trunk/lyngo@3 80cae6e0-d849-4ddc-adcc-ad84e20aa495

--- drillbit/dictedit/ui/imp_dictedit.cpp
+++ drillbit/dictedit/ui/imp_dictedit.cpp
@@ -8,8 +8,19 @@ DictEditWin::DictEditWin (QWidget *parent)
     connect(this->mnuExit, SIGNAL(triggered(bool)),
             QCoreApplication::instance(), SLOT(quit()));
 
+    this->_dictionary = new LyDict();
 }
 
+DictEditWin::~DictEditWin(void)
+{
+    if (this->_dictionary)
+        delete this->_dictionary;
+}
+
+/**
+ * @brief
+ *  Menu open event
+ */
 void DictEditWin::on_mnuOpen_triggered(bool)
 {
     QString fileName = QFileDialog::getOpenFileName(
@@ -24,6 +35,7 @@ void DictEditWin::on_mnuOpen_triggered(bool)
         delete this->_dictionary;
     this->_dictionary = new LyDict();
     this->_dictionary->load(fileName);
+    this->_current_file = fileName;
 
     // propagate data into widgets
     update_widgets();
@@ -40,6 +52,8 @@ void DictEditWin::update_widgets(void)
     this->twWordBank->clear();
     this->leUnitTitle->setText("[Unit Title]");
 
+    if (!this->_current_file.isEmpty())
+        this->update_window_title();
     if (!this->_dictionary->name().isEmpty())
         this->leUnitTitle->setText(this->_dictionary->name());
 
@@ -48,4 +62,87 @@ void DictEditWin::update_widgets(void)
         DEBUG_PRINT(this->_dictionary->itemAt(i).join(", ").toStdString().c_str());
         this->twWordBank->addTopLevelItem(item);
     }
+}
+
+/**
+ * @brief
+ *  Saves the currently open file; if there is no file currently open
+ *  on_mnuSaveAs_triggered() is called to establish one and save
+ */
+void DictEditWin::on_mnuSave_triggered(bool)
+{
+    if (!this->_current_file.isEmpty())
+        this->_dictionary->save(this->_current_file);
+    else
+        on_mnuSaveAs_triggered(false);
+}
+
+void DictEditWin::on_mnuSaveAs_triggered(bool)
+{
+    QString fileName = QFileDialog::getSaveFileName(
+            this,
+            tr("Save dictionary..."),
+            QString(),
+            tr("Lyngo Drillbit (*.ldb)")
+    );
+
+    if (this->_dictionary) {
+        this->_dictionary->save(fileName);
+        this->_current_file = fileName;
+    }
+}
+
+/**
+ * @brief
+ *  Updates the window title to display the application name and current file
+ *  name that is being edited.
+ */
+void DictEditWin::update_window_title(void)
+{
+    QString title;
+    title = "Lyngo Drillbit Editor";
+    if (!this->_current_file.isEmpty())
+        title += " - [" + this->_current_file + "]";
+    this->setWindowTitle(title);
+}
+
+void DictEditWin::on_twWordBank_currentItemChanged(QTreeWidgetItem *newItem,
+        QTreeWidgetItem *oldItem)
+{
+    this->leWord->setText(newItem->text(0));
+    this->cmbPartOfSpeech->setCurrentIndex(
+            cmbPartOfSpeech->findText(newItem->text(1))
+    );
+    this->leDefinition->setText(newItem->text(2));
+}
+
+/**
+ * @brief
+ *  Adds a word to the dictionary.
+ */
+void DictEditWin::on_pbAdd_clicked(void)
+{
+    if (!leWord->text().isEmpty() && !leDefinition->text().isEmpty()) {
+        this->_dictionary->add(leWord->text(),
+                this->_dictionary->posFromName(
+                    cmbPartOfSpeech->currentText()
+                ),
+                leDefinition->text()
+        );
+        update_widgets();
+
+        leWord->clear();
+        leDefinition->clear();
+        cmbPartOfSpeech->setCurrentIndex(0);
+        leWord->setFocus();
+    }
+}
+
+/**
+ * @brief
+ *  Edits the currently highlighted word in the dictionary
+ */
+void DictEditWin::on_pbEdit_clicked(void)
+{
+
 }
--- drillbit/dictedit/ui/imp_dictedit.h
+++ drillbit/dictedit/ui/imp_dictedit.h
@@ -8,12 +8,25 @@ class DictEditWin : public QMainWindow, private Ui::DictEditWin
 
     public:
         DictEditWin(QWidget *parent = 0);
+        ~DictEditWin();
 
     private slots:
+        // menus
         void on_mnuOpen_triggered(bool);
+        void on_mnuSave_triggered(bool);
+        void on_mnuSaveAs_triggered(bool);
+
+        void on_twWordBank_currentItemChanged(QTreeWidgetItem*,
+                QTreeWidgetItem*);
+
+        // buttons
+        void on_pbAdd_clicked(void);
+        void on_pbEdit_clicked(void);
 
     private:
         void update_widgets(void);
+        void update_window_title(void);
 
         LyDict *_dictionary;
+        QString _current_file;
 };
